9ad0e079f9c2388f0f63029cfd142af574d11d83
Мария Плетнева <=>
2024-03-01 00:46:20 +0500

    added currency endpoint

--- src/geo/services/weather.py
+++ src/geo/services/weather.py
@@ -12,7 +12,7 @@ class WeatherService:
 
     def get_weather(self, alpha2code: str, city: str) -> Optional[dict]:
         """
-        Получение списка стран по названию.
+        Получение погоды города.
 
         :param alpha2code: ISO Alpha2 код страны
         :param city: Город
@@ -23,29 +23,3 @@ class WeatherService:
             return data
 
         return None
-
-    def build_model(self, country: CountryDTO) -> Country:
-        """
-        Формирование объекта модели страны.
-
-        :param CountryDTO country: Данные о стране.
-        :return:
-        """
-
-        return Country(
-            alpha3code=country.alpha3code,
-            name=country.name,
-            alpha2code=country.alpha2code,
-            capital=country.capital,
-            region=country.region,
-            subregion=country.subregion,
-            population=country.population,
-            latitude=country.latitude,
-            longitude=country.longitude,
-            demonym=country.demonym,
-            area=country.area,
-            numeric_code=country.numeric_code,
-            flag=country.flag,
-            currencies=[currency.code for currency in country.currencies],
-            languages=[language.name for language in country.languages],
-        )
--- src/geo/urls.py
+++ src/geo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from geo.views import get_city, get_cities, get_countries, get_country, get_weather
+from geo.views import get_city, get_cities, get_countries, get_country, get_weather, get_currency
 
 urlpatterns = [
     path("city", get_cities, name="cities"),
     path("city/<str:name>", get_city, name="city"),
     path("country", get_countries, name="countries"),
     path("country/<str:name>", get_country, name="country"),
+    path("currency/<str:base>", get_currency, name="currency"),
     path("weather/<str:alpha2code>/<str:city>", get_weather, name="weather"),
 ]
